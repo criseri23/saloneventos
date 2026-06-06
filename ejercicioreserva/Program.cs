e48040df959008d154568f0b76b96a38ee129d6c
criseri23 <misselb624@gmail.com>
2026-06-05 22:30:35 -0300

    Add files via upload

---
+++ ejercicioreserva/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Tipo dereserva");
+        Console.WriteLine("1 - Solo Evento");
+        Console.WriteLine("2 - Con Catering");
+        Console.WriteLine("3 - Full");
+
+        int tipoReserva = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Nombre del cliente: ");
+        string nombreCliente = Console.ReadLine();
+
+        Console.Write("Cantidad de invitados: ");
+        int cantidadInvitados = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Cantidad de horas: ");
+        int cantidadHoras = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Incluye mozos (S/N): ");
+        bool incluyeMozos = Console.ReadLine().ToUpper() == "S";
+
+        Console.Write("Dia (V/S/D): ");
+        char dia = Convert.ToChar(Console.ReadLine().ToUpper());
+
+        char tipoMenu = 'N';
+        int cantidadAnimaciones = 0;
+
+        if (tipoReserva == 2 || tipoReserva == 3)
+        {
+            Console.Write("Menu (B/P): ");
+            tipoMenu = Convert.ToChar(Console.ReadLine().ToUpper());
+        }
+
+        if (tipoReserva == 3)
+        {
+            Console.Write("Cantidad de animaciones: ");
+            cantidadAnimaciones = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Reserva reserva1 = new Reserva(
+            nombreCliente,
+            cantidadInvitados,
+            cantidadHoras,
+            incluyeMozos,
+            dia,
+            tipoReserva,
+            tipoMenu,
+            cantidadAnimaciones
+        );
+
+        Console.WriteLine();
+        Console.WriteLine(reserva1.ObtenerResumen());
+
+        Console.ReadLine();
+    }
+}
---
+++ ejercicioreserva/Reserva.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+class Reserva
+{
+    private string nombreCliente;
+    private int cantidadInvitados;
+    private int cantidadHoras;
+    private bool incluyeMozos;
+    private char dia;
+    private int tipoReserva;
+    private char tipoMenu;
+    private int cantidadAnimaciones;
+
+    public const int PrecioSalonHora = 6000;
+    public const int PrecioMozo = 3000;
+    public const int PrecioCateringBasico = 5000;
+    public const int PrecioCateringPremium = 11000;
+    public const int PrecioAnimacion = 18000;
+
+    public Reserva(string nombreCliente, int cantidadInvitados, int cantidadHoras,
+                   bool incluyeMozos, char dia, int tipoReserva,
+                   char tipoMenu, int cantidadAnimaciones)
+    {
+        NombreCliente = nombreCliente;
+        CantidadInvitados = cantidadInvitados;
+        CantidadHoras = cantidadHoras;
+        IncluyeMozos = incluyeMozos;
+        Dia = dia;
+        TipoReserva = tipoReserva;
+        TipoMenu = tipoMenu;
+        CantidadAnimaciones = cantidadAnimaciones;
+    }
+
+    public string NombreCliente
+    {
+        get { return nombreCliente; }
+        set { nombreCliente = value; }
+    }
+
+    public int CantidadInvitados
+    {
+        get { return cantidadInvitados; }
+        set { cantidadInvitados = value; }
+    }
+
+    public int CantidadHoras
+    {
+        get { return cantidadHoras; }
+        set { cantidadHoras = value; }
+    }
+
+    public bool IncluyeMozos
+    {
+        get { return incluyeMozos; }
+        set { incluyeMozos = value; }
+    }
+
+    public char Dia
+    {
+        get { return dia; }
+        set { dia = value; }
+    }
+
+    public int TipoReserva
+    {
+        get { return tipoReserva; }
+        set { tipoReserva = value; }
+    }
+
+    public char TipoMenu
+    {
+        get { return tipoMenu; }
+        set { tipoMenu = value; }
+    }
+
+    public int CantidadAnimaciones
+    {
+        get { return cantidadAnimaciones; }
+        set { cantidadAnimaciones = value; }
+    }
+
+    public decimal CalcularValor()
+    {
+        decimal total = CantidadHoras * PrecioSalonHora;
+
+        if (IncluyeMozos)
+        {
+            int cantidadMozos = (int)Math.Ceiling(CantidadInvitados / 15.0);
+            total += cantidadMozos * PrecioMozo;
+        }
+
+        if (Dia == 'S')
+        {
+            total += total * 0.80m;
+        }
+        else if (Dia == 'D')
+        {
+            total += total * 0.50m;
+        }
+
+        if (TipoReserva == 2 || TipoReserva == 3)
+        {
+            if (TipoMenu == 'B')
+            {
+                total += CantidadInvitados * PrecioCateringBasico;
+            }
+            else if (TipoMenu == 'P')
+            {
+                total += CantidadInvitados * PrecioCateringPremium;
+            }
+        }
+
+        if (TipoReserva == 3)
+        {
+            total += CantidadAnimaciones * PrecioAnimacion;
+        }
+
+        return total;
+    }
+
+    public string ObtenerResumen()
+    {
+        return NombreCliente +
+               " - Inv: " + CantidadInvitados +
+               " - Horas: " + CantidadHoras +
+               " - PRECIO $ " + CalcularValor();
+    }
+}
